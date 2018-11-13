05c78a3169156a06a1029dd075bf1cf844f6f74d
Your NamePertyAlex <a.zabrodskyi@gmail.com>
2018-11-13 09:08:20 +0100

    t6

--- src/main/java/task6.java
+++ src/main/java/task6.java
@@ -1,4 +1,3 @@
-import java.awt.image.ImagingOpException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,46 +5,52 @@ import java.io.InputStreamReader;
 /**
  * Created by Perty on 10-Nov-18.
  */
-public class task6 {
+public class Task6 {
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     double a_side = Double.parseDouble(reader.readLine());
     double b_side = Double.parseDouble(reader.readLine());
     double c_side = Double.parseDouble(reader.readLine());
-    System.out.println(Square(a_side,b_side,c_side));
-    if (IsRectangular(a_side,b_side,c_side))
+    System.out.println(square(a_side, b_side, c_side));
+    if (isRectangular(a_side, b_side, c_side))
       System.out.println("Triagle is rectangular");
     else System.out.println("Triagle isn't rectangular");
 
   }
 
-  private static double Square (double a, double b, double c){
+  private static double square(double a, double b, double c) {
     double p = 0;
     p = (a + b + c)/2;
     return Math.sqrt(p*(p - a) * (p - b) * (p - c));
   }
 
-  private static boolean IsRectangular(double a, double b, double c){
+  private static boolean isRectangular(double a, double b, double c) {
     boolean rectang = true;
-    double max =  MaxValue(a, b, c);
-  if (max == a)
-    rectang = (Math.pow(a,2) == Math.pow(b,2) + Math.pow(c,2));
-  if (max == b)
-    rectang = (Math.pow(b,2) == Math.pow(a,2) + Math.pow(c,2));
-  if (max == c)
-    rectang = (Math.pow(c,2) == Math.pow(b,2) + Math.pow(a,2));
+    double max = maxValue(a, b, c);
+    if (max == a) {
+      rectang = (Math.pow(a, 2) == Math.pow(b, 2) + Math.pow(c, 2));
+    }
+    if (max == b) {
+      rectang = (Math.pow(b, 2) == Math.pow(a, 2) + Math.pow(c, 2));
+    }
+    if (max == c) {
+      rectang = (Math.pow(c, 2) == Math.pow(b, 2) + Math.pow(a, 2));
+    }
     return rectang;
   }
 
-  private static double MaxValue(double a, double b, double c){
+  private static double maxValue(double a, double b, double c) {
     double max = Integer.MIN_VALUE;
-      if (a > max)
+    if (a > max) {
         max = a;
-        if (b > max)
-          max = b;
-           if (c > max)
-            max = c;
+    }
+    if (b > max) {
+      max = b;
+    }
+    if (c > max) {
+      max = c;
+    }
     return max;
   }
 }
